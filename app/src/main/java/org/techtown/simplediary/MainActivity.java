97669e1ef057dbb8c9a2c91971f3d6a142d6b807
xodms1701 <kimrrrrt123@naver.com>
2017-05-30 20:46:54 +0900

    #3 DatePicker Handler done

--- app/src/main/java/org/techtown/simplediary/MainActivity.java
+++ app/src/main/java/org/techtown/simplediary/MainActivity.java
@@ -6,10 +6,13 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
     DatePicker date;
     EditText edit;
     Button but;
+    String fileName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +23,24 @@ public class MainActivity extends AppCompatActivity {
         edit = (EditText)findViewById(R.id.edit);
         but = (Button)findViewById(R.id.but);
 
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DATE);
 
+        date.init(year, month, day, new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker view, int year, int month, int day) {
+                fileName = year+"_"+(month+1)+"_"+day+".txt";
+                String readData = readDiary(fileName);
+                edit.setText(readData);
+                but.setEnabled(true);
+
+            }
+        });
     }
+    public String readDiary(String fileName){
+        return null;
+    }
+
 }
