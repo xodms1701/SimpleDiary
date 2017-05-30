0e572ec4db6dfe3fa72914252b8e6514a8ac4209
xodms1701 <kimrrrrt123@naver.com>
2017-05-30 20:25:30 +0900

    #2 Assign View Refenrence

--- app/src/main/java/org/techtown/simplediary/MainActivity.java
+++ app/src/main/java/org/techtown/simplediary/MainActivity.java
@@ -2,12 +2,24 @@ package org.techtown.simplediary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    DatePicker date;
+    EditText edit;
+    Button but;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        date = (DatePicker)findViewById(R.id.data_pick);
+        edit = (EditText)findViewById(R.id.edit);
+        but = (Button)findViewById(R.id.but);
+
+
     }
 }
