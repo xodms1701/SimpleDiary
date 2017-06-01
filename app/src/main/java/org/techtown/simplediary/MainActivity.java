4676bcea581f5732948b268fcd4107fc43486062
xodms1701 <kimrrrrt123@naver.com>
2017-06-01 19:27:46 +0900

    #5 done

--- app/src/main/java/org/techtown/simplediary/MainActivity.java
+++ app/src/main/java/org/techtown/simplediary/MainActivity.java
@@ -1,13 +1,17 @@
 package org.techtown.simplediary;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Calendar;
 
@@ -25,6 +29,20 @@ public class MainActivity extends AppCompatActivity {
         date = (DatePicker)findViewById(R.id.data_pick);
         edit = (EditText)findViewById(R.id.edit);
         but = (Button)findViewById(R.id.but);
+        but.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    FileOutputStream fOut = openFileOutput(fileName, Context.MODE_PRIVATE);
+                    String str = edit.getText().toString();
+                    fOut.write(str.getBytes());
+                    fOut.close();
+                    Toast.makeText(MainActivity.this, "정상적으로 "+fileName+"파일이 저장되었습니다.",Toast.LENGTH_LONG).show();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
 
         Calendar cal = Calendar.getInstance();
         int year = cal.get(Calendar.YEAR);
@@ -50,11 +68,16 @@ public class MainActivity extends AppCompatActivity {
             fIn.read(buf);
             diaryStr = new String(buf).trim();
             but.setText("수정하기");
+
         } catch (FileNotFoundException e) {
             edit.setText("일기가 존재하지 않습니다. ");
             but.setText("새로 저장");
         } catch (IOException e) {
-
+            try {
+                fIn.close();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
         }
 
         return diaryStr;
