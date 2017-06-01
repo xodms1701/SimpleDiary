8901bf3f4aa9a80573af364e08010ed5f7831dc4
xodms1701 <kimrrrrt123@naver.com>
2017-06-01 19:02:48 +0900

    #4 readDiary() completion

--- app/src/main/java/org/techtown/simplediary/MainActivity.java
+++ app/src/main/java/org/techtown/simplediary/MainActivity.java
@@ -6,6 +6,9 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
@@ -35,12 +38,26 @@ public class MainActivity extends AppCompatActivity {
                 String readData = readDiary(fileName);
                 edit.setText(readData);
                 but.setEnabled(true);
-
             }
         });
     }
     public String readDiary(String fileName){
-        return null;
+        String diaryStr = null;
+        FileInputStream fIn = null;
+        try {
+            fIn = openFileInput(fileName);
+            byte[] buf = new byte[500];
+            fIn.read(buf);
+            diaryStr = new String(buf).trim();
+            but.setText("수정하기");
+        } catch (FileNotFoundException e) {
+            edit.setText("일기가 존재하지 않습니다. ");
+            but.setText("새로 저장");
+        } catch (IOException e) {
+
+        }
+
+        return diaryStr;
     }
 
 }
